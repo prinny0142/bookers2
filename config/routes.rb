63bcb3dcdcc682a4bb5e9d4da52a934fb6f0a0db
prinny0142 <piano.hatsune.lesson@gmail.com>
2020-05-07 19:15:15 +0900

    検索機能を追加した

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
   get 'home/about' => 'home#about'
+  get 'search' => 'search#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :edit, :create, :update] do
     member do
       get :follows
       get :followers
     end
-    get :search, on: :collection
     resource :relationships, only:[:create, :destroy]
   end
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
